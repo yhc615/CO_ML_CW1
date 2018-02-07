7e3323f071a987635e3a29d2904d36c5e2281eef
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-02-07 22:18:14 +0000

    fix mess and add stuff

--- decision_tree.py
+++ decision_tree.py
@@ -3,17 +3,26 @@ Decision-tree Class Definition
 """
 
 class Tree:
-    def __init__(self,label ,i='b'): #i = { 'b':branch , 'l':leaf}, default branch
-        self.kids = []
-        if i == 'b':
-            self.node_label = label
+    def __init__(self,label, isLeaf=False):
+        self.nodeLabel = None
+        self.nodekids = []
+        self.leafLabel = None
+        if isLeaf:
+            self.leafLabel = label
         else:
-            if label in [0,1]:
-                self.leaf_label = label
-            else:
-                raise Exception("Invalid Label for leaf node")
-            
-    def create_child(self, left, right):
-        self.kids = [left, right]
-        
-            
+            self.nodeLabel = label
+
+    def.getKids(self):
+        return self.kids
+
+    def setKid(self, i, node):
+        if k in [0,1]:
+            self.kids[i] = node
+        else:
+            raise Exception("Out of range")
+
+def leaf(x):
+    return Tree(x, True)
+
+def tree(x):
+    return Tree(x)
--- dtl.py
+++ dtl.py
@@ -3,15 +3,6 @@ def MAJORITY_VALUE(inp):
 
 def CHOOSE_BEST_DECISION_ATTR(ex,attr,b_targets):
 	return 0
-<<<<<<< HEAD
-
-def leaf(x):
-	node = Tree("")
-	node.label_leaf(x)
-	return node
-
-def tree(x):
-	return Tree(x)
 
 def DTL(examples, attributes, binary_targets):
 	if(binary_targets[1:] == binary_targets[:-1]):
@@ -24,7 +15,8 @@ def DTL(examples, attributes, binary_targets):
 		for v in [0,1]:
 			v_examples, v_binary_targets = [e for e in examples if e[best_attribute]==v],[binary_targets[i] for i,e in enumerate(examples) if e[best_attribute]==v]
 			if not v_examples:
-				tree.branch[v] = leaf(MAJORITY_VALUE(binary_targets))
+				nodeToAdd = leaf(MAJORITY_VALUE(binary_targets))
 			else:
-				tree.branch[v] = DTL(v_examples,attributes-best_attribute,v_binary_targets)
+				nodeToAdd = DTL(v_examples,attributes-{best_attribute},v_binary_targets)
+			tree.setKid(v, nodeToAdd)
 		return tree
