6ee69e1a79c7411a500e93beeefc77619ce2825b
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-02-07 21:22:02 +0000

    commiting old changes

--- dtl.py
+++ dtl.py
@@ -4,6 +4,14 @@ def MAJORITY_VALUE(inp):
 def CHOOSE_BEST_DECISION_ATTR(ex,attr,b_targets):
 	return 0
 
+def leaf(x):
+	node = Tree("")
+	node.label_leaf(x)
+	return node
+
+def tree(x):
+	return Tree(x)
+
 def DTL(examples, attributes, binary_targets):
 	if(binary_targets[1:] == binary_targets[:-1]):
 		return leaf(binary_targets[0])
@@ -12,11 +20,11 @@ def DTL(examples, attributes, binary_targets):
 	else:
 		best_attribute = CHOOSE_BEST_DECISION_ATTR(examples,attributes,binary_targets)
 		tree = tree(best_attribute)
-		for v in best_attribute:
-			#todo: add branch to tree of v
-			#v_examples, v_binary_targets = 
+		for v in [0,1]:
+			v_examples, v_binary_targets = [e for e in examples if e[best_attribute]==v],[binary_targets[i] for i,e in enumerate(examples) if e[best_attribute]==v]
 			if(not v_examples):
 				return leaf(MAJORITY_VALUE(binary_targets))
 			else:
 				subtree = DTL(v_examples,attributes-best_attribute,v_binary_targets)
+			tree.branch([v] = subtree)
 	return tree
