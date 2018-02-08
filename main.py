6d91a714d32c9fdc6aaa570aad92986de1da3541
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-02-08 13:48:49 +0000

    add main

---
+++ main.py
@@ -0,0 +1,14 @@
+import scipy.io
+from Data import *
+from dtl import *
+from decision_tree import *
+
+raw_data = scipy.io.loadmat('Data/noisydata_students.mat')
+data = Data(raw_data['x'][:10],raw_data['y'][:10])
+data.set_binary_target("Happiness")
+e, b_t = data.to_vector()
+
+attr = list(range(1,46))
+
+print(b_t)
+#dtl = DTL(e, attr, b_t)
