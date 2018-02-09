00b99a92ebbff1dda374444a1a4924c6dd183448
smb00123 <smb00123@users.noreply.github.com>
2018-02-09 10:41:52 +0000

    Cleaned up a little

--- evaluation.py
+++ evaluation.py
@@ -1,17 +1,14 @@
-import numpy
-#t is a tree
 #result is the output vector of the prediction : which emotion was predicted for each example row
 #if examples.y == predicted ---> TP or TN
 #if examples.y != predicted ---> FP or FN
-#one tree for each emotion : output either 1 (positive) or 0 (negative)
+#Vector of predicted outputs for the examples after all 6 trees are input as "predicted"
 
 def confusion_matx(actual, predicted):
     #emotion_label = {'Anger':1, 'Disgust':2, 'Fear':3, 'Happiness':4, 'Sadness':5, 'Surprise':6}
     confusion = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
     recallrate, precisionrate = [0.0,0.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0,0.0]
+    
     for i in range(len(actual)):
-        #emotion = actual result
-        #result = predicted result
         confusion[actual[i]-1][predicted[i]-1] +=1   
     TP,FP,TN,FN =[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]
 
@@ -19,8 +16,7 @@ def confusion_matx(actual, predicted):
         for j in range(6):
             TP[i] = confusion[i][i]
             if j != i:
-                FN[i] += confusion[i][j]
-                
+                FN[i] += confusion[i][j]               
                 FP[i] += confusion[j][i]
                 TN[i] += confusion[j][j]
     for i in range(6):    
@@ -31,8 +27,10 @@ def confusion_matx(actual, predicted):
 
     return confusion, TP, FP, TN, FN, recallrate, precisionrate
 
+#example inputs:
 actual = [1,1,3,6,6,6,1]
 predicted = [2,1,3,6,6,6,6]
+
 conf_matx = confusion_matx(actual, predicted)[0]
 print("Confusion Matrix:")
 for i in range(len(conf_matx)):    
@@ -46,7 +44,6 @@ print(confusion_matx(actual, predicted)[3])
 print("FN")
 print(confusion_matx(actual, predicted)[4])
 print("RECALL RATES")
-
 for element in confusion_matx(actual, predicted)[5]:
     print(element)
 print("PRECISION RATES")
