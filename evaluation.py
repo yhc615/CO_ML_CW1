81921812d37c84113fd1cd57c238dd3159ee78bf
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-02-09 19:53:45 +0000

    spaces to tabs

--- evaluation.py
+++ evaluation.py
@@ -2,28 +2,28 @@
 #if actual != predicted ---> FP or FN
 
 def confusionMatrix(actual, predicted):
-    confusion = [[0]*6,[0]*6,[0]*6,[0]*6,[0]*6,[0]*6]
-    recallrate, precisionrate,F1 = [0]*6,[0]*6,[0]*6
-    for i in range(len(actual)):
-        #emotion = actual result
-        #result = predicted result
-        confusion[actual[i]-1][predicted[i]-1] +=1   
-    TP,FP,TN,FN =[0]*6,[0]*6,[0]*6,[0]*6
+	confusion = [[0]*6,[0]*6,[0]*6,[0]*6,[0]*6,[0]*6]
+	recallrate, precisionrate,F1 = [0]*6,[0]*6,[0]*6
+	for i in range(len(actual)):
+		#emotion = actual result
+		#result = predicted result
+		confusion[actual[i]-1][predicted[i]-1] +=1   
+	TP,FP,TN,FN =[0]*6,[0]*6,[0]*6,[0]*6
 
-    for i in range(6):
-        for j in range(6):
-            TP[i] = confusion[i][i]
-            if j != i:
-                FN[i] += confusion[i][j]
-                
-                FP[i] += confusion[j][i]
-                TN[i] += confusion[j][j]
-    for i in range(6):    
-        if(TP[i] != 0 or FN[i] !=0):
-            recallrate[i] = float(TP[i])/(TP[i]+FN[i])
-        if(TP[i] != 0 or FP[i] !=0):
-            precisionrate[i] = float(TP[i])/(TP[i]+FP[i])
-        if(precisionrate[i] !=0 or recallrate[i] != 0):
-            F1[i] = 2*(precisionrate[i]*recallrate[i])/(precisionrate[i]+recallrate[i])
+	for i in range(6):
+		for j in range(6):
+			TP[i] = confusion[i][i]
+			if j != i:
+				FN[i] += confusion[i][j]
+				
+				FP[i] += confusion[j][i]
+				TN[i] += confusion[j][j]
+	for i in range(6):    
+		if(TP[i] != 0 or FN[i] !=0):
+			recallrate[i] = float(TP[i])/(TP[i]+FN[i])
+		if(TP[i] != 0 or FP[i] !=0):
+			precisionrate[i] = float(TP[i])/(TP[i]+FP[i])
+		if(precisionrate[i] !=0 or recallrate[i] != 0):
+			F1[i] = 2*(precisionrate[i]*recallrate[i])/(precisionrate[i]+recallrate[i])
 
-    return confusion, TP, FP, TN, FN, recallrate, precisionrate, F1
+	return confusion, TP, FP, TN, FN, recallrate, precisionrate, F1
