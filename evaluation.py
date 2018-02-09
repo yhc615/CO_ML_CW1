74993f3d3ea462053cc3f8d5861a1fd6e5452ea5
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-02-09 19:43:20 +0000

    print change

--- evaluation.py
+++ evaluation.py
@@ -2,26 +2,26 @@
 #if actual != predicted ---> FP or FN
 
 def confusionMatrix(actual, predicted):
-    confusion = [[0]*6,[0]*6,[0]*6,[0]*6,[0]*6,[0]*6]
-    recallrate, precisionrate = [0]*6,[0]*6
-    for i in range(len(actual)):
-        #emotion = actual result
-        #result = predicted result
-        confusion[actual[i]-1][predicted[i]-1] +=1   
-    TP,FP,TN,FN =[0]*6,[0]*6,[0]*6,[0]*6
+	confusion = [[0]*6,[0]*6,[0]*6,[0]*6,[0]*6,[0]*6]
+	recallrate, precisionrate = [0]*6,[0]*6
+	for i in range(len(actual)):
+		#emotion = actual result
+		#result = predicted result
+		confusion[actual[i]-1][predicted[i]-1] +=1   
+	TP,FP,TN,FN =[0]*6,[0]*6,[0]*6,[0]*6
 
-    for i in range(6):
-        for j in range(6):
-            TP[i] = confusion[i][i]
-            if j != i:
-                FN[i] += confusion[i][j]
-                
-                FP[i] += confusion[j][i]
-                TN[i] += confusion[j][j]
-    for i in range(6):    
-        if(TP[i] != 0 or FN[i] !=0):
-            recallrate[i] = float(TP[i])/(TP[i]+FN[i])
-        if(TP[i] != 0 or FP[i] !=0):
-            precisionrate[i] = float(TP[i])/(TP[i]+FP[i])
+	for i in range(6):
+		for j in range(6):
+			TP[i] = confusion[i][i]
+			if j != i:
+				FN[i] += confusion[i][j]
+				
+				FP[i] += confusion[j][i]
+				TN[i] += confusion[j][j]
+	for i in range(6):
+		if(TP[i] != 0 or FN[i] !=0):
+			recallrate[i] = float(TP[i])/(TP[i]+FN[i])
+		if(TP[i] != 0 or FP[i] !=0):
+			precisionrate[i] = float(TP[i])/(TP[i]+FP[i])
 
-    return confusion, TP, FP, TN, FN, recallrate, precisionrate
+	return confusion, TP, FP, TN, FN, recallrate, precisionrate
--- main.py
+++ main.py
@@ -36,20 +36,26 @@ def testTrees(T, x2):
 
 def main():
 	raw_data = scipy.io.loadmat('Data/noisydata_students.mat')
-	data = [raw_data['x'],raw_data['y']]
+	data = [raw_data['x'][:100],raw_data['y'][:100]]
 	data[1] = [x[0] for x in data[1]]
 
 	treeSet = genTrees(data)
 	predicts = testTrees(treeSet, data[0])
 	cM = confusionMatrix(data[1], predicts)
-	for x in cM[0]:
-		print(x)
+	#for x in cM:
+		#print(x)
+	i = 0
+	for x in data[1]:
+		if x == 5:
+			i += 1
+	print(i)
 
-	# emotion_labels = {1:'Anger', 2:'Disgust', 3:'Fear', 4:'Happiness', 5:'Sadness', 6:'Surprise'}
-	# for i,x in enumerate(treeSet):
-	# 	print("Emotion: {}".format(emotion_labels[i+1]))
-	# 	print(x)
-	# 	print('-'*100)
+
+	emotion_labels = {1:'Anger', 2:'Disgust', 3:'Fear', 4:'Happiness', 5:'Sadness', 6:'Surprise'}
+	for i,x in enumerate(treeSet):
+		print("Emotion: {}".format(emotion_labels[i+1]))
+		print(x)
+		print('-'*100)
 
 
 if __name__ == "__main__":
