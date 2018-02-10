b57bf5fb06155b821d4a777d34b86bfe9f6dde38
Shrey Bagga <Shreyus@dyn1206-134.wlan.ic.ac.uk>
2018-02-10 18:15:42 +0000

    stats.py integrated

--- evaluation.py
+++ evaluation.py
@@ -14,3 +14,27 @@ def confusionMatrix(actual, predicted):
 	classificationrate = 1-errorrate
 
 	return confusion
+
+def stats(confusion):
+	recallrate, precisionrate,F1 = [0]*6,[0]*6,[0]*6
+	
+	TP,FP,TN,FN =[0]*6,[0]*6,[0]*6,[0]*6
+	
+	for i in range(6):
+		for j in range(6):
+			TP[i] = confusion[i][i]
+			if j != i:
+				FN[i] += confusion[i][j]
+				
+				FP[i] += confusion[j][i]
+				TN[i] += confusion[j][j]
+	for i in range(6):
+		if(TP[i] != 0 or FN[i] !=0):
+			recallrate[i] = float(TP[i])/(TP[i]+FN[i])
+		if(TP[i] != 0 or FP[i] !=0):
+			precisionrate[i] = float(TP[i])/(TP[i]+FP[i])
+		if(precisionrate[i] !=0 or recallrate[i] != 0):
+			F1[i] = 2*(precisionrate[i]*recallrate[i])/(precisionrate[i]+recallrate[i])
+
+
+	return TP, FP, TN, FN, recallrate, precisionrate, F1
